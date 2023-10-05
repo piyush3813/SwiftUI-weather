57c4b7368c7d640e66faff0ea8a78b5cf2e27e3c
piyushXP0067 <piyush@xpindia.in>
2023-10-05 11:57:40 +0530

    Initial Commit

---
+++ SwiftUI-weather/SwiftUI_weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_weatherApp.swift
+//  SwiftUI-weather
+//
+//  Created by XP India  on 05/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
